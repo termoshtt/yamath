c16317764f6cac046f01838025ad6957d0596ed6
Toshiki Teramura <toshiki.teramura@gmail.com>
2015-05-23 05:22:12 +0900

    Add test for AV = VH

--- yamath/test/test_krylov.py
+++ yamath/test/test_krylov.py
@@ -75,3 +75,13 @@ class TestKrylov(TestCase):
             for j in range(i + 1, N):
                 vj = V[:, j]
                 np.testing.assert_array_almost_equal(np.dot(vi, vj), 0.0)
+
+    def test_h(self):
+        """ Confirm AV = VH """
+        N = 5
+        rand = np.random.rand(N, N)
+        A = linalg.LinearOperator((N, N), matvec=lambda x: np.dot(rand, x), dtype=np.float64)
+        b = np.zeros(N)
+        b[0] = 2.0
+        H, V = krylov.arnoldi(A, b)
+        np.testing.assert_almost_equal(A * V, np.dot(V, H))
